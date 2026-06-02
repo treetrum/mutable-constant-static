7d6ef149ee9c083de99fb33b2f7dce0f6f357c55
Sam Davis <sam@sjd.co>
2026-06-03 09:21:30 +1000

    feat: style refresh (#29)

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,5 @@
 export const siteMeta = {
   title: "Mutable Constant",
-  subtitle: "Nerdy things written by some guy called Sam.",
-  description:
-    "A place for Sam Davis to write out some of the scary thoughts he has each day as he lives out his life as a front-end developer.",
+  subtitle: "Development blog for Sam Davis",
+  description: "Development blog for Sam Davis.",
 };
